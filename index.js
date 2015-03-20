c08d2d32398d3386b0f718224d82d4e25acbb3de
C. Corey Capel <corey@capelio.com>
2015-03-20 01:02:12 -0400

    Refactor a bit, clean up coding style

--- index.js
+++ index.js
@@ -1,53 +1,54 @@
-var request = require('superagent'),
-  _ = require('lodash');
+var util = require('util');
+var request = require('superagent');
+var _ = require('lodash');
 
 module.exports = Gnipper;
 
 function Gnipper (options) {
   if (!options) throw new Error('No options supplied to Gnipper constructor');
-  if (!options.account || !options.username || !options.password) {
+
+  if (
+    !options.account ||
+    !options.username ||
+    !options.password
+  ) {
     throw new Error('Missing required option. Required: account, username, password');
   }
 
-  this.account = options.account;
-  this.username = options.username;
-  this.password = options.password;
+  this.options = options;
   this.API = 'https://search.gnip.com/accounts/' + options.account;
 }
 
-Gnipper.prototype.searchUrl = function (options) {
-  var label = options.label || 'prod';
-  var url = this.API + '/search/' + label + '.json?';
-  delete options.label;
+Gnipper.prototype.search = function (query, callback) {
+  if (!query.publisher) return callback(new Error('publisher is a required param for Gnipper.search()'));
 
-  _.each(options, function (value, key) {
-    url = url + key + '=' + encodeURIComponent(value) + '&';
-  });
+  var label = query.label || 'prod';
+  var url = util.format('%s/search/%s.json', this.API, label);
 
-  // Drop the final & and return the URL
-  return url.slice(0, -1);
-};
+  // TODO: Move the label option into the constructor
+  if (query.label) delete query.label;
 
-Gnipper.prototype.search = function (options, callback) {
-  if (!options.publisher) return callback(new Error('publisher is a required param for Gnipper.search()'));
+  var self = this;
 
-  var that = this;
-  var searchUrl = this.searchUrl(options);
-
-  request.get(searchUrl)
-    .auth(this.username, this.password)
-    .end(function (response) {
-      if (response.error) {
+  request
+    .get(url)
+    .query(query)
+    .auth(this.options.username, this.options.password)
+    .end(function (error, response) {
+      if (error) {
         // Include the error details returned by the Gnip API
-        var err = _.merge(response.error, response.body.error);
+        var err = _.merge(error, response.body.error);
+
         // Include Gnipper details with the error as well
         err.gnipper = {
-          searchUrl: searchUrl,
-          username: that.username,
-          passwordIsDefined: (_.isUndefined(that.password)) ? false : true
+          url: url,
+          username: self.options.username,
+          passwordIsDefined: self.options.password ? false : true
         };
+
         return callback(err, response);
       }
+
       callback(null, response);
-  });
+    });
 };
